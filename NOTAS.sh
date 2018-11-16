a959f0df95b3e7c54a5c02b145f55e750a2d46b5
marcllort <mac12llm@gmail.com>
2018-11-16 16:50:08 +0100

    nose

--- NOTAS.sh
+++ NOTAS.sh
@@ -12,12 +12,26 @@ END
 
 
 PARTICIO= https://www.howtogeek.com/106873/how-to-use-fdisk-to-manage-partitions-on-linux/
+Hem mostra que tinc tot el espai ple i no puc fer una particio per els usuaris.
+Comanda= sudo fdisk /dev/sda i despres p.
 
 
-SSH:    ssh-copy-id -i /users/visitor/visitor2/home/visitor2/.ssh/visitor2-key.pub 192.168.25.143  
+
+SSH: Afegeix la key, pero segueix demanant la contrasneya, cal afegirla al client?
+Com puc automatitzar que safegeixi sola despres de crearla, ja que si ho poso a rbac hu fa amb sudo i no marcllort
 
 
-2FA= https://systemoverlord.com/2018/03/03/openssh-two-factor-authentication-but-not-service-accounts.html
+2FA= 
+Ara falla perque tinc posats metodes auotritzacio el public key i no el tinc creat
+Find the line @include common-auth and comment it out by adding a '#' character as the first character on the line. This tells PAM not to prompt for a password.
+
+
+S’utilitzara nomes 2FA per al rol DataStore i la Key nom´es pel Visitor. Tots els altres, utilitzaran
+els dos metodes.????????
+
+https://www.digitalocean.com/community/tutorials/how-to-set-up-multi-factor-authentication-for-ssh-on-ubuntu-16-04
+
+https://systemoverlord.com/2018/03/03/openssh-two-factor-authentication-but-not-service-accounts.html
 
 https://www.techrepublic.com/article/how-to-combine-ssh-key-authentication-and-two-factor-authentication-on-linux/
 
